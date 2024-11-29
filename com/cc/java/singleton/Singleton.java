3b39fe0851cd26c4dd67fc1bbc2d487c52be5ab7
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-11-29 11:25:04 +0000

    Singleton / Iteration 1

---
+++ com/cc/java/singleton/Singleton.java
@@ -0,0 +1,18 @@
+package com.cc.java.singleton;
+
+public class Singleton {
+
+// Objektbildung in der Klasse --> static
+private static Singleton instance;
+
+// Feld, auf das NACH der Objektbildung zugegriffen wird
+private int counter;
+
+// public Singleton(){
+
+// }
+
+
+
+
+}
