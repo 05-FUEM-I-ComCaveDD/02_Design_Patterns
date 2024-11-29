5ccd13a9b69d8e6481245f88c0e19216ab4d199b
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-11-29 11:45:59 +0000

    Singleton / Iteration 2

--- com/cc/java/singleton/App.java
+++ com/cc/java/singleton/App.java
@@ -8,13 +8,38 @@ public class App {
 
         // Klassische Instanziierung
 
-        Singleton singleton1 = new Singleton();
-        Helper.output(singleton1);
-        Singleton singleton2 = new Singleton();
-        Helper.output(singleton2);
-        Singleton singleton3 = new Singleton();
-        Helper.output(singleton3);
+        Singleton obj1 = Singleton.getInstance();
+        Helper.output(obj1);
 
+        obj1.actOnEvent();
+        obj1.actOnEvent();
+        // Events werden kumuliert ... 0 + 2
+        Helper.output(obj1.getCounter());
+        Helper.output("---------------------");
+
+        Singleton obj2 = Singleton.getInstance();
+        Helper.output(obj2);
+
+        obj2.actOnEvent();
+        obj2.actOnEvent();
+        obj2.actOnEvent();
+        // Events werden kumuliert ... 2 + 3
+        Helper.output(obj2.getCounter());
+        Helper.output("---------------------");
+
+
+        Singleton obj3 = Singleton.getInstance();
+        Helper.output(obj3);
+
+        obj3.actOnEvent();
+        obj3.actOnEvent();
+        obj3.actOnEvent();
+        obj3.actOnEvent();
+        obj3.actOnEvent();
+		
+        // Events werden kumuliert ... 5 + 5
+        Helper.output(obj3.getCounter());
+   
 
 
         
--- com/cc/java/singleton/Singleton.java
+++ com/cc/java/singleton/Singleton.java
@@ -8,11 +8,33 @@ private static Singleton instance;
 // Feld, auf das NACH der Objektbildung zugegriffen wird
 private int counter;
 
-// public Singleton(){
+private Singleton(){
 
-// }
+}
 
+// ... in der Klasse
 
+public static Singleton getInstance() {
+    
+    if (instance == null) {
+        return instance = new Singleton();
+    }
+    return instance;   
+    
+    // return instance == null ? instance = new Singleton() : instance;
 
+}
+
+   // ... im Objekt
+
+    // Getter für counter
+    public int getCounter() {
+        return counter;
+    }
+
+    // Event? --> counter  = counter + 1
+    public void actOnEvent() {
+        counter ++;
+    }
 
 }
